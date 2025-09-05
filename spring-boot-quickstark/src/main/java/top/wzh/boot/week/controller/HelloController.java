88416b24cbe44653ff13085fa172a6c96da07b76
Your Name <you@example.com>
2025-09-05 20:13:05 +0800

    feat:在HelloController中新增了功能开关helloSwitch

--- spring-boot-quickstark/src/main/java/top/wzh/boot/week/controller/HelloController.java
+++ spring-boot-quickstark/src/main/java/top/wzh/boot/week/controller/HelloController.java
@@ -1,5 +1,6 @@
 package top.wzh.boot.week.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +12,26 @@ import java.util.List;
  */
 @RestController
 public class HelloController {
+    @Value("${server.feature.helloSwitch}")
+    private boolean isHelloEnabled;
+
+    @Value("${server.feature.closeMsg}")
+    private String closeMsg;
+
     @GetMapping("/hello")
-    public String hello(){
-        return "hello world!";
+    public String hello() {
+        if (isHelloEnabled) {
+            return "接口开放中！欢迎访问我的第一个 Spring Boot 项目";
+        } else {
+            return closeMsg;
+        }
     }
+
+
+//    @GetMapping("/hello")
+//    public String hello(){
+//        return "hello world!";
+//    }
 
     @GetMapping("/list")
     public List<String> list(){
