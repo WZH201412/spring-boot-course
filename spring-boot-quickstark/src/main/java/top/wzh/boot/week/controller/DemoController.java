f54af2369b765f64b17e0ffea9ae506f70e53b6d
Your Name <you@example.com>
2025-09-05 17:38:17 +0800

    feat:新设置API接口链接测试

--- spring-boot-quickstark/src/main/java/top/wzh/boot/week/controller/DemoController.java
+++ spring-boot-quickstark/src/main/java/top/wzh/boot/week/controller/DemoController.java
@@ -1,9 +1,14 @@
 package top.wzh.boot.week.controller;
 
-/**
- *@Author: 王振辉
- *@Date: 2025/9/5
- */
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @Author: 王振辉
+ * @Date: 2025/9/5
+ */
+@RestController
 public class DemoController {
+    public String demo() {
+        return "demo";
+    }
 }
--- spring-boot-quickstark/src/main/java/top/wzh/boot/week/controller/HelloController.java
+++ spring-boot-quickstark/src/main/java/top/wzh/boot/week/controller/HelloController.java
@@ -3,6 +3,8 @@ package top.wzh.boot.week.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /**
  * @Author: 王振辉
  * @Date: 2025/9/5
@@ -12,5 +14,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "hello world!";
+    }
+
+    @GetMapping("/list")
+    public List<String> list(){
+        return List.of("1","2","3");
     }
 }
